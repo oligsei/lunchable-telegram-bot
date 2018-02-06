4ede7698bda5dd01ec2cd70818f6ea6f2c894bcc
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-06 21:26:13 +0200

    try and send a message without a external library

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { Callback, Context, Handler } from 'aws-lambda';
-import * as Telebot from 'telebot';
+import { ClientRequest } from 'http';
+import * as https from 'https';
+import * as querystring from 'querystring';
 
-const bot: Telebot = new Telebot({
-    token: process.env.BOT_ACCESS_TOKEN || ''
-});
+const TELEGRAM_API: string = `api.telegram.org/bot${process.env.BOT_ACCESS_TOKEN}`;
 
 export const handler: Handler = (event: any, context: Context, callback?: Callback) => {
     const chatId: any = JSON.parse(event.body).message.chat.id;
 
     console.log('main.ts:', event, context, callback);
 
-    bot.sendMessage(chatId, JSON.stringify(event));
+    const request: ClientRequest = https.request({
+        method: 'POST',
+        hostname: `${TELEGRAM_API}/sendMessage`
+    });
+
+    request.write(querystring.stringify({
+        chatId,
+        text: 'Hello world'
+    }));
+
+    //bot.sendMessage(chatId, JSON.stringify(event));
 
     callback && callback(null, 'Hello world!');
 };
