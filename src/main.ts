5199ffb051c276d14c175dc947aab1bfdf553127
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-06 20:30:38 +0200

    reply with event data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { Callback, Context, Handler } from 'aws-lambda';
+import * as Telebot from 'telebot';
+
+const bot: Telebot = new Telebot({
+    token: process.env.BOT_ACCESS_TOKEN || ''
+});
 
 export const handler: Handler = (event: any, context: Context, callback?: Callback) => {
+    const chatId: any = JSON.parse(event.body).message.chat.id;
 
     console.log('main.ts:', event, context, callback);
+
+    bot.sendMessage(chatId, JSON.stringify(event));
 
     callback && callback(null, 'Hello world!');
 };
