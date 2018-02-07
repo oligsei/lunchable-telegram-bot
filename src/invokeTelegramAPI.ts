1bb2800183520896af44f44d743309a290f0128d
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-08 01:38:42 +0200

    send Content-Length header

--- src/invokeTelegramAPI.ts
+++ src/invokeTelegramAPI.ts
@@ -2,16 +2,18 @@ import { ClientRequest } from 'http';
 import * as https from 'https';
 import * as querystring from 'querystring';
 
-export const invokeTelegramAPI = (method: string, payload?: {}): void => {
+export const invokeTelegramAPI = (method: string, parameters?: {}): void => {
+    const payload: string = querystring.stringify(parameters);
     const request: ClientRequest = https.request({
         method: 'POST',
         hostname: 'api.telegram.org',
         path: `/bot${process.env.BOT_ACCESS_TOKEN}/${method}`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(payload),
             'Connection': 'close'
         }
     });
-    payload && request.write(querystring.stringify(payload));
+    parameters && request.write(payload);
     request.end();
 };
