57c23d03bb91800704ebbc9f46921b8b6132c669
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-08 01:29:39 +0200

    reverted api gateway to not use a proxy

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
-import { APIGatewayEvent, Context, ProxyCallback, ProxyHandler } from 'aws-lambda';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { invokeTelegramAPI } from './invokeTelegramAPI';
 import { Telegram } from './telegram';
 
-export const handler: ProxyHandler = (event: APIGatewayEvent, context: Context, callback?: ProxyCallback) => {
+export const handler: Handler = (payload: Telegram.Update, context: Context, callback?: Callback) => {
     if (callback === undefined) {
         throw new Error('No callback! Why, lambda, WHYY??');
     }
 
-    if (event.body === null) {
-        return callback(new Error('No body'));
-    }
-
-    const payload: Telegram.Update = JSON.parse(event.body);
-
     if (!payload.message) {
-        return callback(new Error('No message found'));
+        console.error('No message found');
+        return callback();
     }
 
     const {from, chat} = payload.message;
 
     if (from === undefined || from.is_bot) {
-        return callback(new Error('Message is from a bot'));
+        console.error('Message is from a bot');
+        return callback();
     }
 
     // respond only to real users
