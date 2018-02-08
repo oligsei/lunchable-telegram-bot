f43d851a5d85e4893760d3c349981fd13bef7b63
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-08 23:34:15 +0200

    tests for isLunchQuestion

---
+++ src/support/isLunchQuestion.spec.ts
@@ -0,0 +1,33 @@
+import { isLunchQuestion } from './isLunchQuestion';
+
+const SHOULD_REACT_TO: string[] = [
+    'Обед?',
+    'Что с обедом?',
+    'Что с обедом сегодня ?',
+    'что с обедом, господа? сс ',
+    'куда на обед?',
+    'Ребят, так что сегодня с обедом?',
+    'На обед сегодня едем?',
+    'обед по расписанию? куда что как?'
+];
+const SHOULD_NOT_REACT_TO: string[] = [
+    'Г?',
+    'Надо ехать',
+    'Ну вы где?',
+    'У меня митинг ',
+    'Обеду быть !'
+];
+
+describe('isLunchQuestion', () => {
+    for (const trigger of SHOULD_REACT_TO) {
+        test(`should react to ${trigger}`, () => {
+            expect(isLunchQuestion(trigger)).toBe(true);
+        });
+    }
+
+    for (const trigger of SHOULD_NOT_REACT_TO) {
+        test(`should not react to ${trigger}`, () => {
+            expect(isLunchQuestion(trigger)).toBe(false);
+        });
+    }
+});
