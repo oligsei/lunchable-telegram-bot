e8e906c2bf4993f0b3568770fde5a6cbe1532773
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-11 02:22:22 +0200

    rearranged invokeTelegramAPI

--- src/support/invokeTelegramAPI.ts
+++ src/support/invokeTelegramAPI.ts
@@ -3,21 +3,20 @@ import * as https from 'https';
 import * as querystring from 'querystring';
 
 export const invokeTelegramAPI = <T = void>(method: string, parameters?: {}): Promise<T> => {
+    const payload: string = querystring.stringify(parameters);
+    const options: https.RequestOptions = {
+        method: 'POST',
+        hostname: 'api.telegram.org',
+        path: `/bot${process.env.BOT_ACCESS_TOKEN}/${method}`,
+        headers: {
+            'Connection': 'close',
+            'Content-Length': Buffer.byteLength(payload),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+
     return new Promise<T>((resolve) => {
-        const payload: string = querystring.stringify(parameters);
-        const request: ClientRequest = https.request(
-            {
-                method: 'POST',
-                hostname: 'api.telegram.org',
-                path: `/bot${process.env.BOT_ACCESS_TOKEN}/${method}`,
-                headers: {
-                    'Connection': 'close',
-                    'Content-Length': Buffer.byteLength(payload),
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            },
-            () => resolve()
-        );
+        const request: ClientRequest = https.request(options, () => resolve());
         parameters && request.write(payload);
         request.end();
     });
