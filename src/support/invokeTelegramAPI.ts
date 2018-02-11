0196d96fa5165288446d3ce9791417ae19740b57
Alexey Reznikov <oligsei.reznikov@gmail.com>
2018-02-11 02:19:05 +0200

    invokeTelegramAPI should return a promise

--- src/support/invokeTelegramAPI.ts
+++ src/support/invokeTelegramAPI.ts
@@ -2,18 +2,23 @@ import { ClientRequest } from 'http';
 import * as https from 'https';
 import * as querystring from 'querystring';
 
-export const invokeTelegramAPI = (method: string, parameters?: {}): void => {
-    const payload: string = querystring.stringify(parameters);
-    const request: ClientRequest = https.request({
-        method: 'POST',
-        hostname: 'api.telegram.org',
-        path: `/bot${process.env.BOT_ACCESS_TOKEN}/${method}`,
-        headers: {
-            'Connection': 'close',
-            'Content-Length': Buffer.byteLength(payload),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
+export const invokeTelegramAPI = <T = void>(method: string, parameters?: {}): Promise<T> => {
+    return new Promise<T>((resolve) => {
+        const payload: string = querystring.stringify(parameters);
+        const request: ClientRequest = https.request(
+            {
+                method: 'POST',
+                hostname: 'api.telegram.org',
+                path: `/bot${process.env.BOT_ACCESS_TOKEN}/${method}`,
+                headers: {
+                    'Connection': 'close',
+                    'Content-Length': Buffer.byteLength(payload),
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            },
+            () => resolve()
+        );
+        parameters && request.write(payload);
+        request.end();
     });
-    parameters && request.write(payload);
-    request.end();
 };
